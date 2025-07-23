648fe6d696a09fc97ba5b8eebf6f1adea1d71d53
Abdul Ghnai <abdulghani4920@gmail.com>
2025-07-23 09:15:54 -0700

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,42 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Calculator {
+private:
+    int num1;
+    int num2;
+
 public:
-    int a;
-    int b;
-
-    Calculator(int x, int y) : a(x), b(y) {}
-
-    void Addition () {
-        cout << "Sum of a+b: " << a+b << endl;
+    Calculator(int x, int y) {
+        num1 = x;
+        num2 = y;
     }
 
-    void Subtraction () {
-        cout << "Subtraction of a-b: " << a-b << endl;
+    void add() {
+        cout << "Result of addition: " << (num1 + num2) << endl;
     }
 
-    void Multiplication () {
-        cout << "Multiplication of a*b: " << a*b << endl;
+    void subtract() {
+        cout << "Result of subtraction: " << (num1 - num2) << endl;
     }
 
-    void Division () {
-        if (b != 0)
-            cout << "Division of a/b: " << a/b << endl;
-        else
+    void multiply() {
+        cout << "Result of multiplication: " << (num1 * num2) << endl;
+    }
+
+    void divide() {
+        if (num2 == 0) {
             cout << "Cannot divide by zero." << endl;
+        } else {
+            cout << "Result of division: " << (num1 / num2) << endl;
+        }
     }
 };
 
-int main () {
-    cout << "Welcome to Calculator " << endl;
+int main() {
+    cout << "=== Simple Calculator ===" << endl;
 
-    Calculator cal(10, 5);
-    cal.Addition();
-    cal.Subtraction();
-    cal.Multiplication();
-    cal.Division();
+    // Let's say we want to do operations on 10 and 5
+    Calculator calc(10, 5);
+
+    calc.add();
+    calc.subtract();
+    calc.multiply();
+    calc.divide();
 
     return 0;
 }
+
+
 
