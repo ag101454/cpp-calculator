eab398382b3742617b8dddedef55a85a4748f3af
Abdul Ghnai <abdulghani4920@gmail.com>
2025-07-23 08:23:30 -0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cassert>
+#include "Calculator.cpp"  // Include your calculator code
+
+using namespace std;
+
+int main() {
+    Calculator cal(10, 5);
+
+    assert(cal.addition() == 15);
+    assert(cal.subtraction() == 5);
+    assert(cal.multiplication() == 50);
+    assert(cal.division() == 2);
+
+    cout << "All tests passed!" << endl;
+    return 0;
+}
